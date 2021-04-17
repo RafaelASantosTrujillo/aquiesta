34313d41eda5d8b9982693a3b005d011583a78e7
Victor Lascares <vmlascares@gmail.com>
2021-04-17 18:49:19 -0500

    Agregando el DarkMode en la Navegacion

--- src/components/FormSearch.jsx
+++ src/components/FormSearch.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../css/formSearch.css';
 
 const FormSearch = () => {
     return (
         <form className="d-flex">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-light btn-imagen px-3" type="submit">
+            <button className="btn btn-outline-light btn-image px-4" type="submit">
             </button>
         </form>
     );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import DropdownItem from './DropdownItem';
+import Nav from './Nav';
 import FormSearch from './FormSearch';
 
-const states = [
-    "Aguascalientes", "Baja California", "Baja California Sur", "Campeche", "Chiapas", "Chihuahua", "Ciudad de México", "Coahuila", "Colima", "Durango", "Estado de México", "Guanajuato", "Guerrero", "Hidalgo", "Jalisco", "Michoacán", "Morelos", "Nayarit", "Nuevo León", "Oaxaca", "Puebla", "Querétaro", "Quintana Roo", "San Luis Potosí", "Sinaloa", "Sonora", "Tabasco", "Tamaulipas", "Tlaxcala", "Veracruz", "Yucatán", "zacatecas" 
-]
+
 
 const NavBar = () => {
     return (
@@ -15,39 +13,7 @@ const NavBar = () => {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto ms-auto mb-2 mb-lg-0 d-flex justify-content-between">
-                        <li className="nav-item me-4">
-                            <a className="nav-link active" aria-current="page" href="/">Inicio</a>
-                        </li>
-                        <li className="nav-item me-4">
-                            <a className="nav-link" href="https://google.com">Directorio</a>
-                        </li>
-                        <li className="nav-item me-4">
-                            <a className="nav-link" href="https://google.com">Categorias</a>
-                        </li>
-                        <li className="nav-item me-4">
-                            <a className="nav-link" href="https://google.com">Destacados</a>
-                        </li>
-                        <li className="nav-item me-4">
-                            <a className="nav-link" href="https://google.com">Registrar Negocio</a>
-                        </li>
-                        <li className="nav-item me-4 dropdown">
-                            <a className="nav-link dropdown-toggle" href="https://google.com" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Seleccionar Estado
-                            </a>
-                            <ul className="dropdown-menu scrollable-menu" aria-labelledby="navbarDropdown">
-                                {
-                                    states.map(state => {
-                                        return (
-                                            <DropdownItem
-                                                state={state}
-                                            />
-                                        )
-                                    })
-                                }
-                            </ul>
-                        </li>
-                    </ul>
+                    <Nav />
                     <FormSearch />
                 </div>
             </div>
