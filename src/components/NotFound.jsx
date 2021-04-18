6cab6534855be83fb7067c15bcb7d9db27e58414
Rafael Santos <rast.santos@gmail.com>
2021-04-18 12:04:11 -0500

    agregando notfound

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 function NotFound (){
     return(
         <React.Fragment>
-            <p>Página no encontrada</p>
+            <div style={{display:"flex", justifyContent:"center",textAlign:"center"}}>
+                <img src="https://image.freepik.com/vector-gratis/error-404-pagina-no-encontrada-falta-papel_114341-25.jpg" alt="Página no encontrada" width="30%"/>
+            </div>
         </React.Fragment>
     );
 }
